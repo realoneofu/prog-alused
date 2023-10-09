5f3883693397e788ef9949a3dcaecc0fe451274b
realoneofu <erki.kuuses@gmail.com>
2023-10-09 13:51:59 +0300

    testing name math

---
+++ print/examples/ex4.py
@@ -0,0 +1,7 @@
+name = "Erki"
+print(f"Hi {name}!")  # Hi Erki!
+price_without_tax = 20
+print(f"Price with tax is {price_without_tax * 1.2}")
+# Price with tax is 24.0
+print("Sum: {1 + 2 + 3}")  # Sum: {1 + 2 + 3}
+print(f"Sum: {1 + 2 + 3}")  # Sum: 6
